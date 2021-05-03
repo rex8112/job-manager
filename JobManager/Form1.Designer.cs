13e17f3b622b686eb74021aa9f6c4b30df0f0e14
Riley Winkler <rex8112@gmail.com>
2021-05-02 20:31:01 -0500

    Build form

---
+++ JobManager/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace JobManager
+{
+    partial class JobManager
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.departmentList = new System.Windows.Forms.ComboBox();
+            this.inDepartment = new System.Windows.Forms.ListBox();
+            this.inDepartmentLabel = new System.Windows.Forms.Label();
+            this.curTask = new System.Windows.Forms.TextBox();
+            this.startTaskButton = new System.Windows.Forms.Button();
+            this.stopTaskButton = new System.Windows.Forms.Button();
+            this.moveDepartment = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // departmentList
+            // 
+            this.departmentList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.departmentList.FormattingEnabled = true;
+            this.departmentList.Location = new System.Drawing.Point(13, 13);
+            this.departmentList.Name = "departmentList";
+            this.departmentList.Size = new System.Drawing.Size(556, 23);
+            this.departmentList.TabIndex = 0;
+            this.departmentList.SelectedIndexChanged += new System.EventHandler(this.departmentList_SelectedIndexChanged);
+            // 
+            // inDepartment
+            // 
+            this.inDepartment.FormattingEnabled = true;
+            this.inDepartment.ItemHeight = 15;
+            this.inDepartment.Items.AddRange(new object[] {
+            "test",
+            "test2"});
+            this.inDepartment.Location = new System.Drawing.Point(13, 76);
+            this.inDepartment.Name = "inDepartment";
+            this.inDepartment.Size = new System.Drawing.Size(556, 229);
+            this.inDepartment.TabIndex = 2;
+            this.inDepartment.SelectedIndexChanged += new System.EventHandler(this.inDepartment_SelectedIndexChanged);
+            // 
+            // inDepartmentLabel
+            // 
+            this.inDepartmentLabel.AutoSize = true;
+            this.inDepartmentLabel.Location = new System.Drawing.Point(13, 55);
+            this.inDepartmentLabel.Name = "inDepartmentLabel";
+            this.inDepartmentLabel.Size = new System.Drawing.Size(109, 15);
+            this.inDepartmentLabel.TabIndex = 3;
+            this.inDepartmentLabel.Text = "Jobs In Department";
+            this.inDepartmentLabel.Click += new System.EventHandler(this.inDepartmentLabel_Click);
+            // 
+            // curTask
+            // 
+            this.curTask.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.curTask.Location = new System.Drawing.Point(13, 326);
+            this.curTask.Name = "curTask";
+            this.curTask.ReadOnly = true;
+            this.curTask.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.curTask.Size = new System.Drawing.Size(556, 29);
+            this.curTask.TabIndex = 4;
+            this.curTask.Text = "00 - Current Task";
+            // 
+            // startTaskButton
+            // 
+            this.startTaskButton.Location = new System.Drawing.Point(13, 362);
+            this.startTaskButton.Name = "startTaskButton";
+            this.startTaskButton.Size = new System.Drawing.Size(269, 142);
+            this.startTaskButton.TabIndex = 5;
+            this.startTaskButton.Text = "Start Task";
+            this.startTaskButton.UseVisualStyleBackColor = true;
+            this.startTaskButton.Click += new System.EventHandler(this.startTaskButton_Click);
+            // 
+            // stopTaskButton
+            // 
+            this.stopTaskButton.Enabled = false;
+            this.stopTaskButton.Location = new System.Drawing.Point(300, 362);
+            this.stopTaskButton.Name = "stopTaskButton";
+            this.stopTaskButton.Size = new System.Drawing.Size(269, 142);
+            this.stopTaskButton.TabIndex = 6;
+            this.stopTaskButton.Text = "Stop Task";
+            this.stopTaskButton.UseVisualStyleBackColor = true;
+            this.stopTaskButton.Click += new System.EventHandler(this.stopTaskButton_Click);
+            // 
+            // moveDepartment
+            // 
+            this.moveDepartment.Location = new System.Drawing.Point(13, 511);
+            this.moveDepartment.Name = "moveDepartment";
+            this.moveDepartment.Size = new System.Drawing.Size(269, 59);
+            this.moveDepartment.TabIndex = 7;
+            this.moveDepartment.Text = "Move Department";
+            this.moveDepartment.UseVisualStyleBackColor = true;
+            this.moveDepartment.Click += new System.EventHandler(this.moveDepartment_Click);
+            // 
+            // JobManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 637);
+            this.Controls.Add(this.moveDepartment);
+            this.Controls.Add(this.stopTaskButton);
+            this.Controls.Add(this.startTaskButton);
+            this.Controls.Add(this.curTask);
+            this.Controls.Add(this.inDepartmentLabel);
+            this.Controls.Add(this.inDepartment);
+            this.Controls.Add(this.departmentList);
+            this.Name = "JobManager";
+            this.Text = "Job Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox departmentList;
+        private System.Windows.Forms.ListBox inDepartment;
+        private System.Windows.Forms.Label inDepartmentLabel;
+        private System.Windows.Forms.TextBox curTask;
+        private System.Windows.Forms.Button startTaskButton;
+        private System.Windows.Forms.Button stopTaskButton;
+        private System.Windows.Forms.Button moveDepartment;
+    }
+}
+
---
+++ JobManager/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JobManager
+{
+    public partial class JobManager : Form
+    {
+        private int orderNum = 10000;
+        public List<Department> Departments { get; set; }
+        public List<string> departmentNames = new List<string>();
+        public List<string> jobNames = new List<string>();
+        private Department activeDepartment;
+        private Job activeJob;
+        public JobManager()
+        {
+            this.Departments = new List<Department>();
+            InitializeComponent();
+            this.initialize_data();
+            this.departmentList.DataSource = this.departmentNames;
+            this.inDepartment.DataSource = this.jobNames;
+        }
+
+        private void AddDepartment(string name)
+        {
+            List<string> temp_desciptions = new List<string>();
+            temp_desciptions.Add("McDonalds");
+            temp_desciptions.Add("Valvoline");
+            temp_desciptions.Add("LaQuinta");
+            Department new_department = new Department(name);
+            this.Departments.Add(new_department);
+            this.departmentNames.Add(name);
+            Order order = new Order(orderNum);
+            for (int i = 0; i < 10; i++)
+            {
+                Job job = new Job(orderNum * 10 + i, temp_desciptions[i % 3], order, new_department);
+                order.AddJob(job);
+            }
+            orderNum++;
+        }
+
+        private void departmentList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.activeDepartment = Departments[this.departmentList.SelectedIndex];
+            this.jobNames.Clear();
+            foreach (Job job in this.activeDepartment.jobs)
+            {
+                this.jobNames.Add(string.Format("{0, 5:D5} - {1}", job.ID, job.Description));
+            }
+            this.inDepartment.DataSource = null;
+            this.inDepartment.DataSource = this.jobNames;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inDepartmentLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inDepartment_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                this.activeJob = this.activeDepartment.jobs[this.inDepartment.SelectedIndex];
+                this.curTask.Text = this.activeJob.ActiveTask.FormattedString;
+            }
+            catch (Exception)
+            {
+                this.activeJob = null;
+                this.curTask.Text = "No Jobs";
+            }
+        }
+
+        private void initialize_data()
+        {
+            this.AddDepartment("Welding");
+            this.AddDepartment("Painting");
+            this.AddDepartment("Fabrication");
+            this.AddDepartment("Final");
+            this.AddDepartment("Packaging");
+
+            foreach (Department department in this.Departments)
+            {
+                List<Job> tmp_jobs = new List<Job>(department.jobs);
+                foreach (Job job in tmp_jobs)
+                {
+                    if (job.Tasks.Count == 0)
+                    {
+                        job.FillTasks(this.Departments);
+                    }
+                }
+                if (department.Name != "Welding")
+                    department.jobs.Clear();
+            }
+
+        }
+
+        private void startTaskButton_Click(object sender, EventArgs e)
+        {
+            this.activeJob.ActiveTask.StartTask();
+            this.startTaskButton.Enabled = false;
+            this.stopTaskButton.Enabled = true;
+            this.inDepartment.Enabled = false;
+            this.departmentList.Enabled = false;
+        }
+
+        private void stopTaskButton_Click(object sender, EventArgs e)
+        {
+            this.activeJob.ActiveTask.StopTask();
+            this.startTaskButton.Enabled = true;
+            this.stopTaskButton.Enabled = false;
+            this.inDepartment.Enabled = true;
+            this.departmentList.Enabled = true;
+        }
+
+        private void moveDepartment_Click(object sender, EventArgs e)
+        {
+            this.activeJob.NextDepartment();
+            this.jobNames.Clear();
+            foreach (Job job in this.activeDepartment.jobs)
+            {
+                this.jobNames.Add(string.Format("{0, 5:D5} - {1}", job.ID, job.Description));
+            }
+            this.inDepartment.DataSource = null;
+            this.inDepartment.DataSource = this.jobNames;
+        }
+    }
+}
