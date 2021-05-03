d24491d7f224b279883fe1ba7be440d02a486da4
Riley Winkler <rex8112@gmail.com>
2021-05-02 20:30:54 -0500

    Create Department.cs

---
+++ JobManager/Department.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Department
+{
+	public string Name { get { return this.name; } }
+	string name;
+	public List<Job> jobs = new List<Job>();
+	public Department(string name)
+	{
+		this.name = name;
+	}
+
+	public void AddJob(Job job)
+	{
+		if (!this.jobs.Contains(job))
+		{
+			this.jobs.Add(job);
+		}
+	}
+
+	public void RemoveJob(Job job)
+	{
+		this.jobs.Remove(job);
+	}
+}
