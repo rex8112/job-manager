af86506d5330089ff556a4181167db9554ece5fb
Riley Winkler <rex8112@gmail.com>
2021-05-02 20:30:47 -0500

    Create Order.cs

---
+++ JobManager/Order.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Order
+{
+	public int ID
+    {
+		get { return this.ID; }
+    }
+	private int id;
+
+	public List<Job> Jobs
+    {
+		get { return this.jobs; }
+    }
+	private List<Job> jobs = new List<Job>();
+	public Order(int orderID)
+	{
+		this.id = orderID;
+	}
+
+	public void AddJob(Job job)
+    {
+		if (!this.jobs.Contains(job))
+        {
+			this.jobs.Add(job);
+        }
+    }
+
+	public void RemoveJob(Job job)
+    {
+		this.jobs.Remove(job);
+    }
+}
